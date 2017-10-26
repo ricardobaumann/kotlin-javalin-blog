cc5b6da38f21f91dba48581e0816b72e3f3a5b67
Ricardo Baumann <gemini.richard@gmail.com>
2017-10-26 15:41:29 +0200

    routes refac

--- src/main/kotlin/blog/Main.kt
+++ src/main/kotlin/blog/Main.kt
@@ -8,15 +8,19 @@ val postService = PostService(Repository<Post>())
 fun main(args: Array<String>) {
     val app = Javalin.start(7000)
     app.routes {
-        get("/posts") { ctx ->
-            ctx.json(postService.getPosts())
-        }
-        post("/posts") { ctx ->
-            val post = ctx.bodyAsClass(Post::class.java)
-            val id = postService.create(post)
 
-            ctx.json(mapOf(Pair("id",id))).status(201)
-            //ctx.status(204)
-        }
+        path("/posts", {
+            get { ctx ->
+                ctx.json(postService.getPosts())
+            }
+            post { ctx ->
+                val post = ctx.bodyAsClass(Post::class.java)
+                val id = postService.create(post)
+
+                ctx.json(mapOf(Pair("id",id))).status(201)
+            }
+        })
+
+
     }
 }
